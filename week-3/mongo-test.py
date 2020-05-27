9c859cc21e32acd6e1aa4fb9f48b9021a9189203
Dondidoo <65118437+Dondidoo@users.noreply.github.com>
2020-05-27 22:31:57 +0900

    week3_2

---
+++ week-3/mongo-test.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta 
+
+## 코딩 할 준비 ##
+
+target_movie = db.movies.find_one({'title':'매트릭스'})
+target_star = target_movie['star']
+
+movies = list(db.movies.find({'star':target_star}))
+
+for movie in movies:
+    title = movie['title']
+    db.movies.update_one({'title':title},{'$set':{'star':0}})
